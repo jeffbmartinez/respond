26d43b34b58834151a70e58172ab4b739f74cc07
jeff martinez <jeffbmartinez@gmail.com>
2016-04-24 10:29:20 -0700

    Always build before pushing code :p Also added a few more reason phrases

--- respond.go
+++ respond.go
@@ -8,28 +8,36 @@ import (
 /*
 ReasonPhrases is a map of status codes to reason phrases
 as recommended by RFC 2616 section 6.1.1
-https://www.ietf.org/rfc/rfc2616.txt
+(https://www.ietf.org/rfc/rfc2616.txt) or a reasonable string
+if not present in the RFC.
 */
 var ReasonPhrase map[int]string
 
 func init() {
 	ReasonPhrase = map[int]string{
-		http.StatusContinue:                     "Continue",
-		http.StatusSwitchingProtocols:           "Switching Protocols",
-		http.StatusContinue:                     "OK",
-		http.StatusCreated:                      "Created",
-		http.StatusAccepted:                     "Accepted",
-		http.StatusNonAuthoritativeInfo:         "Non-Authoritative Information",
-		http.StatusNoContent:                    "No Content",
-		http.StatusResetContent:                 "Reset Content",
-		http.StatusPartialContent:               "Partial Content",
-		http.StatusMultipleChoices:              "Multiple Choices",
-		http.StatusMovedPermanently:             "Moved Permanently",
-		http.StatusFound:                        "Found",
-		http.StatusSeeOther:                     "See Other",
-		http.StatusNotModified:                  "Not Modified",
-		http.StatusUseProxy:                     "Use Proxy",
-		http.StatusTemporaryRedirect:            "Temporary Redirect",
+		// 1xx: Informational
+		http.StatusContinue:           "Continue",
+		http.StatusSwitchingProtocols: "Switching Protocols",
+
+		// 2xx: Success
+		http.StatusOK:                   "OK",
+		http.StatusCreated:              "Created",
+		http.StatusAccepted:             "Accepted",
+		http.StatusNonAuthoritativeInfo: "Non-Authoritative Information",
+		http.StatusNoContent:            "No Content",
+		http.StatusResetContent:         "Reset Content",
+		http.StatusPartialContent:       "Partial Content",
+
+		// 3xx: Redirection
+		http.StatusMultipleChoices:   "Multiple Choices",
+		http.StatusMovedPermanently:  "Moved Permanently",
+		http.StatusFound:             "Found",
+		http.StatusSeeOther:          "See Other",
+		http.StatusNotModified:       "Not Modified",
+		http.StatusUseProxy:          "Use Proxy",
+		http.StatusTemporaryRedirect: "Temporary Redirect",
+
+		// 4xx: Client Error
 		http.StatusBadRequest:                   "Bad Request",
 		http.StatusUnauthorized:                 "Unauthorized",
 		http.StatusPaymentRequired:              "Payment Required",
@@ -48,12 +56,20 @@ func init() {
 		http.StatusUnsupportedMediaType:         "Unsupported Media Type",
 		http.StatusRequestedRangeNotSatisfiable: "Requested range not satisfiable",
 		http.StatusExpectationFailed:            "Expectation Failed",
-		http.StatusInternalServerError:          "Internal Server Error",
-		http.StatusNotImplemented:               "Not Implemented",
-		http.StatusBadGateway:                   "Bad Gateway",
-		http.StatusServiceUnavailable:           "Service Unavailable",
-		http.StatusGatewayTimeout:               "Gateway Time-out",
-		http.StatusHTTPVersionNotSupported:      "HTTP Version not supported",
+		http.StatusTeapot:                       "I'm a teapot",
+		http.StatusPreconditionRequired:         "Precondition Required",
+		http.StatusTooManyRequests:              "Too Many Requests",
+		http.StatusRequestHeaderFieldsTooLarge:  "Request Header Fields Too Large",
+		http.StatusUnavailableForLegalReasons:   "Unavailable For Legal Reasons",
+
+		// 5xx: Server Error
+		http.StatusInternalServerError:           "Internal Server Error",
+		http.StatusNotImplemented:                "Not Implemented",
+		http.StatusBadGateway:                    "Bad Gateway",
+		http.StatusServiceUnavailable:            "Service Unavailable",
+		http.StatusGatewayTimeout:                "Gateway Time-out",
+		http.StatusHTTPVersionNotSupported:       "HTTP Version not supported",
+		http.StatusNetworkAuthenticationRequired: "Network Authentication Required",
 	}
 }
 
@@ -79,6 +95,9 @@ statusCode.
 
 For example, Status(w, http.StatusBadRequest) is the equivalent
 of String(w, "Bad Request", http.StatusBadRequest)
+
+If the statusCode is not recognized (i.e. is not in the ReasonPhrase
+map) an empty string is used.
 */
 func Simple(w http.ResponseWriter, statusCode int) {
 	String(w, ReasonPhrase[statusCode], statusCode)
@@ -104,5 +123,5 @@ HTML sends an html string as the response.
 */
 func HTML(w http.ResponseWriter, message string, statusCode int) {
 	w.Header().Add("Content-Type", "text/html")
-	Simple(w, message, statusCode)
+	String(w, message, statusCode)
 }
