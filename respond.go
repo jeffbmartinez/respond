d9009d4ee3dd318eb7f1161af85e2214da55bc32
jeff martinez <jeffbmartinez@gmail.com>
2016-07-08 09:07:26 -0700

    Add HTMLTemplate function

--- respond.go
+++ respond.go
@@ -1,7 +1,9 @@
 package respond
 
 import (
+	"bytes"
 	"encoding/json"
+	"html/template"
 	"net/http"
 )
 
@@ -124,4 +126,25 @@ HTML sends an html string as the response.
 func HTML(w http.ResponseWriter, message string, statusCode int) {
 	w.Header().Add("Content-Type", "text/html")
 	String(w, message, statusCode)
+}
+
+/*
+HTMLTemplate renders an html template and sends an html string as the response. Will return an error
+if the template was not found or otherwise had a problem. In the case of an error,
+the http.ResponseWriter object will not be written to or modified and it is the developer's
+responsibility to take appropriate action, such as respond.Simple(w, http.StatusInternalServerError)
+*/
+func HTMLTemplate(w http.ResponseWriter, templateName string, data interface{}, statusCode int) error {
+	t, err := template.ParseFiles(templateName)
+	if err != nil {
+		return err
+	}
+
+	var b bytes.Buffer
+	if err := t.Execute(&b, data); err != nil {
+		return err
+	}
+
+	HTML(w, b.String(), statusCode)
+	return nil
 }
