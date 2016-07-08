2a98d75dfdfa15e4963e3e0121abae1a38907e1d
jeff martinez <jeffbmartinez@gmail.com>
2016-07-08 15:38:24 -0700

    return result of ResponseWriter.Write in calls

--- respond.go
+++ respond.go
@@ -9,17 +9,20 @@ import (
 
 /*
 ByteSlice send a byte slice as a response.
+
+Returns the result of calling Write on the http.ResponseWriter interface, this
+is most likely the number of bytes written and any error.
 */
-func ByteSlice(w http.ResponseWriter, message []byte, statusCode int) {
+func ByteSlice(w http.ResponseWriter, message []byte, statusCode int) (int, error) {
 	w.WriteHeader(statusCode)
-	w.Write(message)
+	return w.Write(message)
 }
 
 /*
 String sends a string as your response.
 */
-func String(w http.ResponseWriter, message string, statusCode int) {
-	ByteSlice(w, []byte(message), statusCode)
+func String(w http.ResponseWriter, message string, statusCode int) (int, error) {
+	return ByteSlice(w, []byte(message), statusCode)
 }
 
 /*
@@ -32,36 +35,35 @@ of String(w, "Bad Request", http.StatusBadRequest)
 
 If the statusCode is not recognized by http.StatusText an empty string is used.
 */
-func Simple(w http.ResponseWriter, statusCode int) {
-	String(w, http.StatusText(statusCode), statusCode)
+func Simple(w http.ResponseWriter, statusCode int) (int, error) {
+	return String(w, http.StatusText(statusCode), statusCode)
 }
 
 /*
 JSON sends a json-serializable object as the response and sets
 "Content-Type" header to "application/json".
 
-If there is an error the error is returned and the Content-Type is not set, the
+If there is an error it is returned and the Content-Type is not set, the
 http.ResponseWriter object is not written to or modified.
 */
-func JSON(w http.ResponseWriter, message interface{}, statusCode int) error {
+func JSON(w http.ResponseWriter, message interface{}, statusCode int) (int, error) {
 	jsonAsByteSlice, err := json.Marshal(message)
 
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	w.Header().Add("Content-Type", "application/json")
-	ByteSlice(w, jsonAsByteSlice, statusCode)
-	return nil
+	return ByteSlice(w, jsonAsByteSlice, statusCode)
 }
 
 /*
 HTML sends an html string as the response and sets
 "Content-Type" header to "text/html".
 */
-func HTML(w http.ResponseWriter, message string, statusCode int) {
+func HTML(w http.ResponseWriter, message string, statusCode int) (int, error) {
 	w.Header().Add("Content-Type", "text/html")
-	String(w, message, statusCode)
+	return String(w, message, statusCode)
 }
 
 /*
@@ -73,17 +75,16 @@ In the case of an error, the http.ResponseWriter object will not be written to o
 modified. The developer should take appropriate action, such as
 respond.Simple(w, http.StatusInternalServerError)
 */
-func HTMLTemplate(w http.ResponseWriter, templateName string, data interface{}, statusCode int) error {
+func HTMLTemplate(w http.ResponseWriter, templateName string, data interface{}, statusCode int) (int, error) {
 	t, err := template.ParseFiles(templateName)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	var b bytes.Buffer
 	if err := t.Execute(&b, data); err != nil {
-		return err
+		return 0, err
 	}
 
-	HTML(w, b.String(), statusCode)
-	return nil
+	return HTML(w, b.String(), statusCode)
 }
